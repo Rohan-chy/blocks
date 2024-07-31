4af9b1fb667cc5d0c75dc208365bc074216b312e
rohan-chy <chyrohan036@gmail.com>
2024-07-31 16:41:45 +0545

    block added

---
+++ src/global/Scroller.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Scroller = () => {
+  return (
+    <div className='flex items-center justify-center gap-2 mt-2'>
+    <div className='w-[240px] h-[6px] bg-[#F3F4F6] rounded-full'></div>
+    <div className='w-[480px] h-[6px] rounded-full bg-black'></div>
+    <div className='w-[240px] h-[6px] bg-[#F3F4F6] rounded-full'></div>
+
+</div>
+  )
+}
+
+export default Scroller
